1d52706afe300f212945ccef2ce6fc7b8a0522ce
preethamkh <blighter.soul@gmail.com>
2016-10-10 15:39:11 +1100

    SportsStore-ASPNet MVC

---
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Abstract
+{
+    // REPOSITORY PATTERN
+    // A class that depends on this IProductsRepository interface can obtain Product objects without needing to know anything about where they are coming from or how the implementation class will deliver them
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
---
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using System.Web.Mvc;
+using SportsStore.WebUI.Models;
+using SportsStore.WebUI.HtmlHelpers;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Can_Paginate()
+        {
+            // arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+            new Product {ProductID = 1, Name = "P1"},
+            new Product {ProductID = 2, Name = "P2"},
+            new Product {ProductID = 3, Name = "P3"},
+            new Product {ProductID = 4, Name = "P4"},
+            new Product {ProductID = 5, Name = "P5"}
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // act
+            IEnumerable<Product> result = (IEnumerable<Product>) controller.List(2).Model;
+
+            // assert
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            // Arrange - define an HTML helper - need to do this in order to apply extension method
+            HtmlHelper myHelper = null;
+
+            // Arrange - create PagingInfo data
+            PagingInfo pagingInfo = new PagingInfo()
+            {
+                CurrentPage =  2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            // Arrange - set up the deleage using a lambda expresesion
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            // Act
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            // Assert
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1<a>" + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>", result.ToString());
+        }
+    }
+}

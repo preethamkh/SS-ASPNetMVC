3ef82d06d1501acb8bb880790737608e351f6364
preethamkh <blighter.soul@gmail.com>
2016-10-28 16:52:50 +1100

    corrected password box

---
+++ SportsStore.WebUI/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.WebUI.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
